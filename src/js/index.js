62ef8b4000e871f22f53127b1346153faba53b57
Guido Schmidt <git@guidoschmidt.cc>
2020-11-24 22:43:21 +0100

    WIP/src/js: started building info layer for details about the artwork

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+// Style imports
+import "../sass/index.sass";
 
 const Artwork = () => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <div>
-      <h1>Artwork</h1>
-    </div>
+    <>
+      <div className="canvas-wrapper">
+        <canvas />
+        <button onClick={() => setShowDetails(!showDetails)}>i</button>
+      </div>
+      <div
+        className={["layout-artwork", showDetails ? "visible" : "hidden"].join(
+          " "
+        )}
+      >
+        <h1>Artwork</h1>
+        <p>Details</p>
+      </div>
+    </>
   );
 };
 
