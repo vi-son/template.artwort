38dacce0d203638135ee70084fe2f92781cec1d9
Guido Schmidt <git@guidoschmidt.cc>
2020-11-28 11:53:41 +0100

    EDIT/src/js/index.js: improve details interaction, styling using rouge

--- src/js/index.js
+++ src/js/index.js
@@ -17,13 +17,12 @@ import "../sass/index.sass";
 
 const Artwork = () => {
   const [showDetails, setShowDetails] = useState(false);
-  const [blocks, setBlocks] = useState([]);
+  const [content, setContent] = useState({});
 
   useEffect(() => {
     get(`/pages/audiovis-io`).then(d => {
-      // setContent(d);
-      setBlocks(d.content.blocks);
-      console.log(d.content.blocks);
+      setContent(d.content);
+      console.log(d.content);
     });
   }, []);
 
@@ -31,49 +30,68 @@ const Artwork = () => {
     <>
       <div className="canvas-wrapper">
         <canvas />
-        <button onClick={() => setShowDetails(!showDetails)}>i</button>
+        <button
+          className="emoji btn-details"
+          onClick={() => setShowDetails(!showDetails)}
+        >
+          📖
+        </button>
       </div>
+      <button
+        className={[
+          "btn-close-details",
+          "emoji",
+          showDetails ? "visible" : "hidden"
+        ].join(" ")}
+        onClick={() => setShowDetails(false)}
+      >
+        👈
+      </button>
       <div
         className={["layout-artwork", showDetails ? "visible" : "hidden"].join(
           " "
         )}
       >
-        <h1>Artwork</h1>
-        <p>Details</p>
-        {blocks.map(block => {
-          switch (block._key) {
-            case "textblock":
-              return <TextBlock key={block._uid} content={block} />;
-              break;
-            case "imageblock":
-              return <ImageBlock key={block._uid} content={block} />;
-              break;
-            case "imagegrid":
-              return <ImageGridBlock key={block._uid} content={block} />;
-              break;
-            case "audioblock":
-              return <AudioBlock key={block._uid} content={block} />;
-              break;
-            case "references":
-              return <ReferencesBlock key={block._uid} content={block} />;
-              break;
-            case "videoembed":
-              return <VideoEmedBlock key={block._uid} content={block} />;
-              break;
-            case "hyperlinks":
-              return <HyperlinkBlock key={block._uid} content={block} />;
-              break;
-            case "citeblock":
-              return <CiteBlock key={block._uid} content={block} />;
-              break;
-            case "podcastblock":
-              return <PodcastBlock key={block._uid} content={block} />;
-              break;
-            default:
-              return <section key={block._uid}>{block._key}</section>;
-              break;
-          }
-        })}
+        <h1>{content.title}</h1>
+        <artticle>{content.shortdesription}</artticle>
+        {content.blocks ? (
+          content.blocks.map(block => {
+            switch (block._key) {
+              case "textblock":
+                return <TextBlock key={block._uid} content={block} />;
+                break;
+              case "imageblock":
+                return <ImageBlock key={block._uid} content={block} />;
+                break;
+              case "imagegrid":
+                return <ImageGridBlock key={block._uid} content={block} />;
+                break;
+              case "audioblock":
+                return <AudioBlock key={block._uid} content={block} />;
+                break;
+              case "references":
+                return <ReferencesBlock key={block._uid} content={block} />;
+                break;
+              case "videoembed":
+                return <VideoEmedBlock key={block._uid} content={block} />;
+                break;
+              case "hyperlinks":
+                return <HyperlinkBlock key={block._uid} content={block} />;
+                break;
+              case "citeblock":
+                return <CiteBlock key={block._uid} content={block} />;
+                break;
+              case "podcastblock":
+                return <PodcastBlock key={block._uid} content={block} />;
+                break;
+              default:
+                return <section key={block._uid}>{block._key}</section>;
+                break;
+            }
+          })
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
